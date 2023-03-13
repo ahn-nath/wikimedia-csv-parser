f5b41724e790638cf565aba89fd107f35b81fa15
Nathaly Toledo <nathaly12toledo@gmail.com>
2023-03-13 13:51:03 -0400

    fix: changed delimiter and checks logic to fix errors

--- main.py
+++ main.py
@@ -8,18 +8,23 @@ import re
 PATH = 'yaml_files'
 # TODO: handle the second list case later
 DENY_LIST = ['yaml_files/mt-defaults.wikimedia.yaml', 'yaml_files/MWPageLoader.yaml', 'yaml_files/languages.yaml',
-             'yaml_files/JsonDict.yaml', 'yaml_files/Dictd.yaml', 'yaml_files/transform.js'] + [
-                'yaml_files/Google.yaml', 'yaml_files/Yandex.yaml']
+             'yaml_files/JsonDict.yaml', 'yaml_files/Dictd.yaml', 'yaml_files/transform.js']
+#+ [
+#                'yaml_files/Google.yaml', 'yaml_files/Yandex.yaml']
 
 
 def get_preferred_engines():
-    """ Get the preferred engines from the mt-defaults.wikimedia.yaml file and saves them in a list.
-    It uses pickling to use available files when already processed and save memory.
+    """
 
-    The output would look like this:
-     {'af-nl': 'Apertium', 'ar-mt': 'Apertium', 'be-ru': 'Apertium', 'bg-mk': 'Apertium'...}
+        Get the preferred engines from the mt-defaults.wikimedia.yaml file and saves them in a list.
+        It uses pickling to use available files when already processed and save memory.
 
-    TODO: add return value and enrich docstring
+        The output would look like this:
+         {'af-nl': 'Apertium', 'ar-mt': 'Apertium', 'be-ru': 'Apertium', 'bg-mk': 'Apertium'...}
+
+
+        :return: a dictionary with the preferred engines
+
     """
     # check if file exists
     try:
@@ -48,26 +53,32 @@ def get_preferred_engines():
 
 # generate the CSV file
 def generate_csv(preferred_engines):
-    """ It parses specific files and generates a CSV file with the data that at least includes the source language,
-    the target language, the translation engine used, and whether or not the translation engine is preferred or not
+    """
+        It parses specific files and generates a CSV file with the data that at least includes the source language,
+        the target language, the translation engine used, and whether or not the translation engine is preferred or not
 
-    # TODO: add return value, parameters, and enrich docstring
+        :return: cvs strings generated for the cx_server_parsed.csv file
+        :parameter: preferred_engines: a dictionary with the preferred engines
+
     """
     # parse each file
-    for file in os.listdir(PATH):
+    # list for the lines of the CSV file
+    csv_strings = ["source language,target language,translation engine,is preferred engine?"]
+    for f in os.listdir('yaml_files'):
         # parse file
-        with open(file, 'rb') as file:
-            if file not in DENY_LIST:
+        with open(f'yaml_files/{f}') as file:
+            if file.name not in DENY_LIST:
                 lines = file.readlines()
 
         # get the translation engine used
         cvs_pairs_dict = {}
-        engine = file.name.split('.')[0]
-        standard = False if "languages:" in lines[2] else True  # TODO: this depends on whether the file has a languages key or not
+        engine = re.split("\W+", file.name)[1]
+        standard = False if "languages:" in lines[1] else True
         # if standard
         if standard:
             for line in lines:
                 if line:
+                    target = ''
                     # get the source language
                     if ":" in line:
                         source = line.split(':')[0].strip()
@@ -75,7 +86,8 @@ def generate_csv(preferred_engines):
                     else:
                         target = line.split('-')[1].strip()
 
-                    cvs_pairs_dict["{}-{}".format(source, target)] = engine
+                    if source and target:
+                        cvs_pairs_dict["{}:{}".format(source, target)] = engine
 
         else:
             # languages list
@@ -89,40 +101,30 @@ def generate_csv(preferred_engines):
                     # if the target language is not the source language and it's not in the not_as_target list,
                     # and it's not an english variant
                     if (lang != target) and (target not in not_as_target) and (lang and target not in english_variants):
-                        cvs_pairs_dict["{}-{}".format(lang, target)] = engine
+                        cvs_pairs_dict["{}:{}".format(lang, target)] = engine
 
-    # list for the lines of the CSV file
-    csv_strings = []
-    # iterate over dictionary and handle each case
-    for key, value in cvs_pairs_dict.items():
-        # get the source and target languages from key, check if the value or engine is the preferred to construct
-        source, key = key.split('-')
-        is_preferred = 'true' if preferred_engines.get(f'{source}-{target}') == engine else 'false'
-        csv_string = f"{source},{target},{engine},{is_preferred}"
+        # iterate over dictionary and handle each case
+        for key, value in cvs_pairs_dict.items():
+            # get the source and target languages from key, check if the value or engine is the preferred to construct
+            source, target = key.split(':', 2)
+            # we will mark as true if the engine is the preferred one in the preferred_engines dictionary
+            is_preferred = 'true' if preferred_engines.get(f'{source}-{target}') == engine else 'false'
+            csv_string = f"{source},{target},{engine},{is_preferred}"
 
-        # TODO: we will append to list but later write to file
-        csv_strings.append(csv_string)
+            csv_strings.append(csv_string)
 
     # after processing all the files, write the list to a CSV file
-    # TODO: change to CSV format and add header
-    with open('cx_server_parsed.txt', 'w') as f:
+    with open('cx_server_parsed.csv', 'w') as f:
         f.writelines([f"{x}\n" for x in csv_strings])
 
     return csv_strings
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('started')
     # get the preferred engines
     preferred_engines_out = get_preferred_engines()
-    print(preferred_engines_out)
-
     # generate the CSV file
     csv_strings_out = generate_csv(preferred_engines_out)
+    # print output
     print(csv_strings_out)
