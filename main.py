8c4141e416abe991df207881604da80fd36e1df7
Nathaly Toledo <nathaly12toledo@gmail.com>
2023-03-13 09:34:00 -0400

    fix: added files, improved logic of main.py to generate CSV file
    Improved the logic of the main.py file  and its generate_csv function to include the logic for handling non-standard inputs. Also added a test file to check the code health and efficiency in a simpler way.

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import os
 import pickle
 
 # define constants
+import re
+
 PATH = 'yaml_files'
 # TODO: handle the second list case later
 DENY_LIST = ['yaml_files/mt-defaults.wikimedia.yaml', 'yaml_files/MWPageLoader.yaml', 'yaml_files/languages.yaml',
-             'yaml_files/JsonDict.yaml', 'yaml_files/Dictd.yaml'] + ['yaml_files/Google.yaml', 'yaml_files/Yandex.yaml']
+             'yaml_files/JsonDict.yaml', 'yaml_files/Dictd.yaml', 'yaml_files/transform.js'] + [
+                'yaml_files/Google.yaml', 'yaml_files/Yandex.yaml']
 
 
 def get_preferred_engines():
@@ -58,24 +61,52 @@ def generate_csv(preferred_engines):
                 lines = file.readlines()
 
         # get the translation engine used
+        cvs_pairs_dict = {}
         engine = file.name.split('.')[0]
-        csv_string = ''
-        csv_strings = []
-        for line in lines:
-            if line:
-                # get the source language
-                if ":" in line:
-                    source = line.split(':')[0].strip()
-                # get the target language
-                else:
-                    target = line.split('-')[1].strip()
+        standard = False if "languages:" in lines[2] else True  # TODO: this depends on whether the file has a languages key or not
+        # if standard
+        if standard:
+            for line in lines:
+                if line:
+                    # get the source language
+                    if ":" in line:
+                        source = line.split(':')[0].strip()
+                    # get the target language
+                    else:
+                        target = line.split('-')[1].strip()
 
-                # get whether the translation engine is preferred or not
-                if source and target:
-                    is_preferred = 'true' if preferred_engines.get(f'{source}-{target}') == engine else 'false'
-                    csv_string = f"{source},{target},{engine},{is_preferred}"
-                    # TODO: we will append to list but later write to file
-                    csv_strings.append(csv_string)
+                    cvs_pairs_dict["{}-{}".format(source, target)] = engine
+
+        else:
+            # languages list
+            # regex to remove non-alphabetic characters
+            languages = re.sub(r'[^a-zA-Z]', '', lines[2:]).split(',')
+            # restrictions
+            english_variants = ['en', 'simple']
+            not_as_target = []
+            for lang in languages:
+                for target in languages:
+                    # if the target language is not the source language and it's not in the not_as_target list,
+                    # and it's not an english variant
+                    if (lang != target) and (target not in not_as_target) and (lang and target not in english_variants):
+                        cvs_pairs_dict["{}-{}".format(lang, target)] = engine
+
+    # list for the lines of the CSV file
+    csv_strings = []
+    # iterate over dictionary and handle each case
+    for key, value in cvs_pairs_dict.items():
+        # get the source and target languages from key, check if the value or engine is the preferred to construct
+        source, key = key.split('-')
+        is_preferred = 'true' if preferred_engines.get(f'{source}-{target}') == engine else 'false'
+        csv_string = f"{source},{target},{engine},{is_preferred}"
+
+        # TODO: we will append to list but later write to file
+        csv_strings.append(csv_string)
+
+    # after processing all the files, write the list to a CSV file
+    # TODO: change to CSV format and add header
+    with open('cx_server_parsed.txt', 'w') as f:
+        f.writelines([f"{x}\n" for x in csv_strings])
 
     return csv_strings
 
@@ -88,5 +119,10 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('started')
+    # get the preferred engines
     preferred_engines_out = get_preferred_engines()
     print(preferred_engines_out)
+
+    # generate the CSV file
+    csv_strings_out = generate_csv(preferred_engines_out)
+    print(csv_strings_out)
---
+++ test.py
@@ -0,0 +1,6 @@
+# Unit tests for main.py
+
+# test the generate file with standard input
+# test the generate file with non-standard input
+# test the get preferred engines with the default file
+
